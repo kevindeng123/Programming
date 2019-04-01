48dbd061beb5f178d1303befde2d6fcbfc9a379f
kevindeng <654081343@qq.com>
2019-04-01 21:46:18 +0800

    Update test.py

--- PersonalProject/test.py
+++ PersonalProject/test.py
@@ -1,10 +1,21 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
+# test1
 x = np.arange(1, 11)
 y = 2 * x + 5
 plt.title("Matplotlib demo")
 plt.xlabel("x axis caption")
 plt.ylabel("y axis caption")
 plt.plot(x, y)
+plt.show()
+
+# test2
+N = 50
+x = np.random.rand(N)
+y = np.random.rand(N)
+colors = np.random.rand(N)
+area = (30 * np.random.rand(N))**2
+
+plt.scatter(x, y, s=area, c=colors, alpha=0.5)
 plt.show()
