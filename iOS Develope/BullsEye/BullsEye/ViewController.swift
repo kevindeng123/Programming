ad7e317538547e9f4c5b1a591194f4223ad8056d
kevindeng <654081343@qq.com>
2019-05-04 17:19:15 +0800

    Update

--- iOS Develope/BullsEye/BullsEye/ViewController.swift
+++ iOS Develope/BullsEye/BullsEye/ViewController.swift
@@ -10,11 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var currentValue:Int = 50 // 定义了一个变量
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func showAlert(){
+        //1.设置message信息的内容
+        let message = "滑动条当前的数值：\(currentValue)"
+        //2.设置alert信息的内容
+        let alert = UIAlertController(title: "这是标题", message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "这又是标题", style: .default, handler: nil)
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    }
 
+    @IBAction func sliderMoved(_ slider: UISlider){
+        //print("滑动条的当前数值：\(slider.value)")
+        currentValue = lroundf(slider.value)
+    }
 }
 
