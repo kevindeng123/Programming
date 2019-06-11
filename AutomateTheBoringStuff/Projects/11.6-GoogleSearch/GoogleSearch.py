eb4eb64beb4ec713008246e49ae311793cd75824
Jing Wei Deng <jingwei.deng@morrow.local>
2019-06-11 18:01:38 +0800

    Update Learning

--- AutomateTheBoringStuff/Projects/11.6-GoogleSearch/GoogleSearch.py
+++ AutomateTheBoringStuff/Projects/11.6-GoogleSearch/GoogleSearch.py
@@ -11,10 +11,10 @@ res = requests.get('http://google.com/search?q=' + ' '.join(sys.argv[1:]))
 res.raise_for_status()
 
 # Retrieve top search result links.
-soup = bs4.BeautifulSoup(res.text)
+soup = bs4.BeautifulSoup(res.text, features="html5lib")
 
 # Open a browser tab for each result.
-linkElems = soup.select(' .r a')
+linkElems = soup.select('.r a')
 numOpen = min(5, len(linkElems))
 for i in range(numOpen):
     webbrowser.open('http://google.com' + linkElems[i].get('href'))
---
+++ AutomateTheBoringStuff/Projects/11.7-downloadXkcd/downloadXkcd.py
@@ -0,0 +1,21 @@
+#! python3
+# downloadXkcd.py -  Downloads every single XKCD comic.
+
+import requests
+import os
+import bs4
+
+url = 'http://xkcd.com'
+os.makedirs('xkcd', exist_ok=True)
+while not url.endswith('#'):
+    # Download the page.
+
+    # Find the URL of the comic image.
+
+    # Download the image.
+
+    # Save the image to ./xkcd.
+
+    # Get the Prev button's url.
+
+print('Done.')
