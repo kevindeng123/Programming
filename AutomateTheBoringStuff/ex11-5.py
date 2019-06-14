53c4e110cb7a26f368809748f9592d36bc5a3550
Jing Wei Deng <jingwei.deng@morrow.local>
2019-06-14 20:22:27 +0800

    Learning

--- AutomateTheBoringStuff/ex11-5.py
+++ AutomateTheBoringStuff/ex11-5.py
@@ -2,11 +2,15 @@ import requests
 import bs4
 import os
 
+# Change current directory
 print(os.getcwd())
 abspath = os.path.abspath(__file__)
+print(abspath)
 dname = os.path.dirname(abspath)
 os.chdir(dname)
 print(os.getcwd())
+targetPath = r"C:\Users\jingwei.deng\Documents\xkcd"
+print(os.path.join(targetPath, 'xkcd'))
 
 exampleFile = open(r'.\automate_online-materials\example.html')
 exampleSoup = bs4.BeautifulSoup(exampleFile, features="html5lib")
